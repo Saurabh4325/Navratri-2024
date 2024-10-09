c10a997d32ac0b7cd6630dd88a038d5ef1cce161
Saurabh Kumar <sbhk4325@gmail.com>
2024-10-10 05:17:13 +0530

    changes in footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Home from './pages/Home';
 import Classmates from './components/Classmates/Classmates';
 import NavratriNav from './components/Navbar/NavratriNav';
 import HumanFacesGrid from './components/HumanFacesGrid/HumanFacesGrid';
+import Footer from './components/Footer/Footer';
+
 
 
 
@@ -15,6 +17,7 @@ const App = () => {
         <Route path="/Navratri-2024" element={<Home />} />
         <Route path="/classmates" element={<HumanFacesGrid />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
--- src/components/HumanFacesGrid/HumanFacesGrid.jsx
+++ src/components/HumanFacesGrid/HumanFacesGrid.jsx
@@ -44,7 +44,7 @@ const HumanFacesGrid = () => {
           </div>
         ))}
       </div>
-    </div>
+      </div>
   );
 };
 
